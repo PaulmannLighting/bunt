03f49d00bbf1f58a533ed03aedfbe709e922c28d
Richard Neumann <neumann@paulmann.de>
2026-07-01 20:29:09 +0200

    Add common RGB color constants.

--- src/rgb.rs
+++ src/rgb.rs
@@ -8,6 +8,30 @@ pub struct Rgb {
 }
 
 impl Rgb {
+    /// Plain red.
+    pub const RED: Self = Self::new(0xff, 0x00, 0x00);
+
+    /// Plain green.
+    pub const GREEN: Self = Self::new(0x00, 0xff, 0x00);
+
+    /// Plain blue.
+    pub const BLUE: Self = Self::new(0x00, 0x00, 0xff);
+
+    /// Cyan (aka aqua).
+    pub const CYAN: Self = Self::new(0x00, 0xff, 0xff);
+
+    /// Magenta.
+    pub const MAGENTA: Self = Self::new(0xff, 0x00, 0xff);
+
+    /// Yellow.
+    pub const YELLOW: Self = Self::new(0xff, 0xff, 0x00);
+
+    /// White.
+    pub const WHITE: Self = Self::new(0xff, 0xff, 0xff);
+
+    /// Black (kinda...).
+    pub const BLACK: Self = Self::new(0x00, 0x00, 0x00);
+
     /// Create a new `Rgb` color.
     #[must_use]
     pub const fn new(red: u8, green: u8, blue: u8) -> Self {
